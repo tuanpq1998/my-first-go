76b2eb5e0639c837002b37e1e70548574101354b
tuanpq1998 <tuanquang123321@gmail.com>
2025-02-24 22:31:30 +0700

    add TODO note

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -4,6 +4,8 @@ import "net/http"
 
 func AllowCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// TODO: the below settings always be writen even it is error,
+		//  it isnt the same as when use go-chi/cors library
 		w.Header().Set("Access-Control-Allowed-Origins", "*")
 		w.Header().Set("Access-Control-Allowed-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allowed-Headers", "*")
